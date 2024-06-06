5a128cea26b9edc676ad5a7dd40551c2bb761f8b
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-06 22:52:30 +0300

    Added admin panel settings

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
 from transactions.models import Transaction
 
@@ -6,5 +7,8 @@ from transactions.models import Transaction
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ("user", "type_transaction", "amount", "created_at", "updated_at",)
-    list_filter = ("created_at",)
-    search_fields = ("type_transaction", "created_at",)
+    list_filter = (
+        ("created_at", DateFieldListFilter),
+        ("updated_at", DateFieldListFilter),
+    )
+    search_fields = ("type_transaction", "created_at", "updated_at")
