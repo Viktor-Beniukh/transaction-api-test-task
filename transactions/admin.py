89ebb7796be61f67645bbc3999570ae84872f870
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-06 18:51:54 +0300

    Rename Transaction admin model

--- transactions/admin.py
+++ transactions/admin.py
@@ -4,7 +4,7 @@ from transactions.models import Transaction
 
 
 @admin.register(Transaction)
-class ArticleAdmin(admin.ModelAdmin):
+class TransactionAdmin(admin.ModelAdmin):
     list_display = ("user", "type_transaction", "amount", "created_at", "updated_at",)
     list_filter = ("created_at",)
     search_fields = ("type_transaction", "created_at",)
