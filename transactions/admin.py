d6d4472c450b1688e8c310f21e6a7381d99bfad3
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-06 18:50:57 +0300

    Added Transaction model to admin.py

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from transactions.models import Transaction
+
+
+@admin.register(Transaction)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("user", "type_transaction", "amount", "created_at", "updated_at",)
+    list_filter = ("created_at",)
+    search_fields = ("type_transaction", "created_at",)
