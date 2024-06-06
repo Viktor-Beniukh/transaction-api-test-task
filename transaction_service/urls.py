31939c6e9806f1270960ad086d67289af8e53d0c
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-06 20:28:55 +0300

    Realised transactions endpoints

--- transaction_service/urls.py
+++ transaction_service/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("user.urls", namespace="user")),
+    path("api/v1/", include("transactions.urls", namespace="transactions")),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from transactions.models import Transaction
+from transactions.serializers import TransactionSerializer, TransactionListSerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.select_related("user")
+    serializer_class = TransactionSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminUser,)
+
+    def get_queryset(self):
+        """Retrieve the transaction with filter"""
+        type_transaction = self.request.query_params.get("type_transaction")
+
+        queryset = super().get_queryset()
+
+        if type_transaction:
+            queryset = queryset.filter(type_transaction__icontains=type_transaction)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return TransactionListSerializer
+
+        return super().get_serializer_class()
